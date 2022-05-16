3c14f9d536efaf98a5cb3e1af2c41ea075fcafe1
B635 <frankxck@gmail.com>
2022-05-16 16:16:01 +0800

    Update: 更新主程序, 添加扫描Mapper

--- ticket_backend/src/main/java/com/example/ticket_backend/TicketBackendApplication.java
+++ ticket_backend/src/main/java/com/example/ticket_backend/TicketBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.ticket_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.ticket_backend.mapper")
 public class TicketBackendApplication {
 
 	public static void main(String[] args) {
