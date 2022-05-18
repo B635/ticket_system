35f77f57b29a10f3e6937688b32852475aaec83e
B635 <frankxck@gmail.com>
2022-05-18 22:56:44 +0800

    Update: 更新用户车票信息相关类

---
+++ ticket_backend/src/main/java/com/example/ticket_backend/controller/userTicketController.java
@@ -0,0 +1,27 @@
+package com.example.ticket_backend.controller;
+
+import com.example.ticket_backend.bean.userTicket;
+import com.example.ticket_backend.service.userTicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class userTicketController {
+    @Autowired
+    userTicketService userTicketService;
+
+    @ResponseBody
+    @RequestMapping(value = "/addTicket", method = RequestMethod.POST)
+    public void addTicket(
+            @RequestBody Map<String, String> body
+    ) {
+        userTicket result = new userTicket(body.get("account"), body.get("trainNo"), body.get("seatClass"));
+        userTicketService.addTicket(result);
+    }
+}
--- ticket_backend/src/main/java/com/example/ticket_backend/mapper/userTicketMapper.java
+++ ticket_backend/src/main/java/com/example/ticket_backend/mapper/userTicketMapper.java
@@ -1,9 +1,10 @@
 package com.example.ticket_backend.mapper;
 
 import com.example.ticket_backend.bean.userTicket;
+import org.apache.ibatis.annotations.Param;
 
 public interface userTicketMapper {
 
-    void addTicket(userTicket userTicket);
+    void addTicket(@Param("userTicket") userTicket userTicket);
 
 }
